4449a20b199d0ed20d75e9bad5382a46dabb6386
Willem <91901507+wverbuyst@users.noreply.github.com>
2024-05-27 04:41:51 +0200

    Add search to band overview

--- src/app/_components/band/Overview.tsx
+++ src/app/_components/band/Overview.tsx
@@ -10,12 +10,33 @@ export default function Overview({
   bands: Awaited<ReturnType<typeof api.band.getAll>>;
 }) {
   const searchParams = useSearchParams();
-  const genreSearchParam = searchParams.get("genre");
-  const genres = genreSearchParam?.split(",") ?? [];
+  const genreParam = searchParams.get("genre");
+  const searchParam = searchParams.get("search");
+  const genres = genreParam?.split(",") ?? [];
 
   const bandsFilteredByGenre = bands.filter((band) => {
-    if (!genres.length) return true;
-    return band.genre.some((g) => genres.includes(g));
+    if (!genres.length && !searchParam) {
+      return true;
+    }
+
+    const searchString = searchParam?.toLowerCase() ?? "";
+    const bandName = band.name.toLowerCase();
+    const bandBio = band.bio?.toLowerCase() ?? "";
+    const bandGenres = band.genre.map((g) => g.toLowerCase());
+    if (!genres.length) {
+      return (
+        bandName.includes(searchString) ||
+        bandBio.includes(searchString) ||
+        bandGenres.includes(searchString)
+      );
+    }
+
+    return (
+      band.genre.some((g) => genres.includes(g)) &&
+      (bandName.includes(searchString) ||
+        bandBio.includes(searchString) ||
+        bandGenres.includes(searchString))
+    );
   });
 
   return (
--- src/app/bands/page.tsx
+++ src/app/bands/page.tsx
@@ -1,6 +1,7 @@
 import { api } from "~/trpc/server";
 import GenreFilter from "../_components/GenreFilter";
 import PageTitle from "../_components/PageTitle";
+import Search from "../_components/Search";
 import Overview from "../_components/band/Overview";
 
 export default async function Page() {
@@ -10,6 +11,7 @@ export default async function Page() {
     <div className="flex min-h-screen flex-col items-center gap-8">
       <PageTitle title="Bands" />
       <GenreFilter />
+      <Search />
       <Overview bands={bands} />
     </div>
   );
