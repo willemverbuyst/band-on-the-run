0353da9e7177936d148e0bf60141cc541efdb80a
Willem <91901507+wverbuyst@users.noreply.github.com>
2024-06-10 06:28:57 +0200

    Remove icon in dashboard title

--- src/components/dashboard/dashboard-menu-title.tsx
+++ src/components/dashboard/dashboard-menu-title.tsx
@@ -1,11 +1,10 @@
 import Link from "next/link";
-import { Icons } from "../icons";
 
 export default function DashboardMenuTitle() {
   return (
     <h4>
       <Link href="/dashboard" className="flex items-center">
-        <Icons.fileBarChart className="text-primary" /> Dashboard
+        Dashboard
       </Link>
     </h4>
   );
