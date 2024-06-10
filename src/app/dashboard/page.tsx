730a22393a9c66b473511b36b2e2a942d1c0ea43
Willem <91901507+wverbuyst@users.noreply.github.com>
2024-06-10 18:10:27 +0200

    Add tab component to dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,3 +1,14 @@
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "~/components/ui/tabs";
+
 export default function DashboardPage() {
-  return <div>Dashboard</div>;
+  return (
+    <Tabs defaultValue="shows">
+      <TabsList>
+        <TabsTrigger value="shows">Shows stats</TabsTrigger>
+        <TabsTrigger value="bands">Bands stats</TabsTrigger>
+      </TabsList>
+      <TabsContent value="shows">Shows view</TabsContent>
+      <TabsContent value="bands">Bands view</TabsContent>
+    </Tabs>
+  );
 }
